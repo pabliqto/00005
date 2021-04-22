7cb6bdd2fbb5bca858b9cb2ba7c6ed36b1ae70f4
Pablito <41995400+paweello3@users.noreply.github.com>
2021-04-22 17:55:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+function o1()
+{
+    document.getElementById("odp1").innerHTML="Literę M";
+}
+function c1()
+{
+    document.getElementById("odp1").innerHTML="...";
+}
+
+function o2()
+{
+    document.getElementById("odp2").innerHTML="Oddechem";
+}
+function c2()
+{
+    document.getElementById("odp2").innerHTML="...";
+}
+
+function o3()
+{
+    document.getElementById("odp3").innerHTML="Wszystkie zwierzęta są albo na łóżku albo latają  więc jedyne nogi na ziemi są to twoje nogi i nogi łóżka łącznie 6 nóg.";
+}
+function c3()
+{
+    document.getElementById("odp3").innerHTML="...";
+}
+
+function o4()
+{
+    document.getElementById("odp4").innerHTML="Rzuciłeś piłeczkę pionowo do góry.";
+}
+function c4()
+{
+    document.getElementById("odp4").innerHTML="...";
+}
+
+function o5()
+{
+    document.getElementById("odp5").innerHTML="Człowiek";
+}
+function c5()
+{
+    document.getElementById("odp5").innerHTML="...";
+}
